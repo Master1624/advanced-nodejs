0ef3e826b13a0069c9a2d77340eca56b083b0ced
Master1624 <jdpa83@gmail.com>
2025-07-18 13:17:41 -0500

    feat: Pino basis for login

--- logger/index.js
+++ logger/index.js
@@ -1,16 +1,51 @@
+const pino = require("pino");
+const isProduction = process.env.NODE_ENV === "production";
+
+const developmentConfig = {
+  level: "debug",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: "pid,hostname",
+      translateTime: "HH:MM:ss",
+      levelFirst: true,
+    },
+  },
+};
+
+const productionConfig = {
+  level: "info",
+  transport: {
+    target: "pino/file",
+    options: {
+      destination: "app.log",
+      translateTime: "yyyy-MM-dd HH:MM:ss Z",
+      levelFirst: true,
+    },
+  },
+};
+
+const config = isProduction ? productionConfig : developmentConfig;
+const logger = pino(config);
+
 function operacion() {
+  let k = 1;
   setInterval(() => {
-    console.log("Realizando operación...");
+    logger.debug({
+      message: "Realizando operacion...",
+      contador: k++,
+    });
   }, 1000);
 }
 
 process.on("uncaughtException", (err) => {
-  console.error("Error no capturado: ", err);
+  logger.error("Error no capturado: ", err);
   process.exit(1);
 });
 
 process.on("unhandledRejection", (reason) => {
-  console.error("Rechazo no capturado:", reason);
+  logger.error("Rechazo no capturado:", reason);
   process.exit(1);
 });
 
@@ -18,7 +53,7 @@ const signals = ["SIGINT", "SIGTERM", "SIGUSR1", "SIGUSR2"];
 
 signals.forEach((signal) => {
   process.on(signal, () => {
-    console.log(`Recibido ${signal}`);
+    logger.info(`Recibido ${signal}`);
     process.exit(0);
   });
 });
